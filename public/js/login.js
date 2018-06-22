1425f6a376997998dee4d88fe415d6012c339fc3
ramyalshurafa <ramyshurafa@hotmail.com>
2018-06-22 23:09:55 +0300

    create login files and create token for admin Relates#3

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginForm=document.getElementById('cp-login-form')
+const username=document.getElementById('username')
+const password=document.getElementById('password')
+loginForm.addEventListener('submit',function(e){
+  e.preventDefault()
+  fetch('/admin/login',{
+   headers: { 'content-type': 'application/json' },
+   method: 'POST',
+   credentials: 'include',
+   body: JSON.stringify({
+     username: username.value,
+     password:password.value
+   })
+ })
+    .then(res=>res.json())
+    .then(res=>{
+      console.log(res);
+      if (res.err) {
+        swal("Error!", res.err.errMsg, "error");
+      }else if(res.rediect){
+        window.location=res.rediect
+      }
+    })
+})
